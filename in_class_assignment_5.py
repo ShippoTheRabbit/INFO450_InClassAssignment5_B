ee64aaa1f773a83eaa6d36bab2b1006df131b358
Boston <Shippotheboss3@gmail.com>
2022-10-27 19:21:27 -0400

    Adding in the classcode example to try
    Classcode example to try next time

--- in_class_assignment_5.py
+++ in_class_assignment_5.py
@@ -78,3 +78,25 @@ def main():
 if __name__ == "__main__":
     print(main())
     
+
+
+"""
+IN Class Code Answer: Mine still works though : 3
+
+def partition(array, low, high){
+    pivot = array[high]
+    i = low-1
+    for j in range(low,high):
+        i = low - 1
+        (array[i], array[j]) = (array[j],array[i])
+    (array[i+1],array[high]) = (array[high],array[i+1]) 
+    return i + 1 
+}
+def quicksort(array,low,high){
+    if low < high 
+        pi = partition(array,low,high)
+        quicksort(array,low,pi-1)
+        quicksort(array, pi+1, high)
+        return array
+}
+"""
